dc5503e19c659ee28b0688ed89c8aaedad9b3a18
MoisesRos <95959492+MoisesRos@users.noreply.github.com>
2024-04-24 15:14:05 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+******************************************************************************
+Universidad del Valle de Guatemala
+Programación de Microcrontroladores
+Proyecto: Lab 4
+Archivo: main.asm
+Hardware: ATMEGA328p
+Author : Moises Rosales
+******************************************************************************
+ */ 
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
